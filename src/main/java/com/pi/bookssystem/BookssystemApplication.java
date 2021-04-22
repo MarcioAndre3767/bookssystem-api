eca63ecb52b71b05f67622953be5e1ede6a21f38
MarcioAndre3767 <marcioandre.ti1@gmail.com>
2021-04-22 06:58:58 -0300

    commit inicial

---
+++ src/main/java/com/pi/bookssystem/BookssystemApplication.java
@@ -0,0 +1,13 @@
+package com.pi.bookssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookssystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/bookssystem/BookssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.bookssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookssystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
